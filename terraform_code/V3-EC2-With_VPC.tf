6db5eb21bed3e701219726180aaad2985b04ecd8
bernard <benmotu@yahoo.com>
2023-10-11 10:28:52 -0500

    added Version3

--- terraform_code/V3-EC2-With_VPC.tf
+++ terraform_code/V3-EC2-With_VPC.tf
@@ -1,50 +1,48 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"
 }
 
 resource "aws_instance" "demo-server" {
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
     key_name = "dpp"
-    //security_groups = [ "demo-sg" ]
+    //security_groups = ["demo-sg"]
     vpc_security_group_ids = [aws_security_group.demo-sg.id]
-    subnet_id = aws_subnet.dpp-public-subnet-01.id 
-
+    subnet_id = aws_subnet.dpp-public-subnet-01.id
+ 
 }
+
 
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
-  vpc_id = aws_vpc.dpp-vpc.id 
-  
+  vpc_id = aws_vpc.dpp-vpc.id
+    
   ingress {
     description      = "Shh access"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    }
+  }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "ssh-prot"
-
+    Name = "ssh-port"
   }
 }
 
 resource "aws_vpc" "dpp-vpc" {
-  cidr_block = "10.1.0.0/16"
-  tags = {
-    Name = "dpp-vpc"
-  }
-  
+    cidr_block = "10.1.0.0/16"
+    tags = {
+      Name = "dpp-vpc"
+    }
 }
 
 resource "aws_subnet" "dpp-public-subnet-01" {
@@ -53,7 +51,7 @@ resource "aws_subnet" "dpp-public-subnet-01" {
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1a"
   tags = {
-    Name = "dpp-public-subent-01"
+    Name = "dpp-public-subnet-01"
   }
 }
 
@@ -63,31 +61,32 @@ resource "aws_subnet" "dpp-public-subnet-02" {
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1b"
   tags = {
-    Name = "dpp-public-subent-02"
+    Name = "dpp-public-subnet-02"
   }
 }
 
 resource "aws_internet_gateway" "dpp-igw" {
-  vpc_id = aws_vpc.dpp-vpc.id 
+  vpc_id = aws_vpc.dpp-vpc.id
   tags = {
     Name = "dpp-igw"
-  } 
+  }
 }
 
 resource "aws_route_table" "dpp-public-rt" {
-  vpc_id = aws_vpc.dpp-vpc.id 
+  vpc_id = aws_vpc.dpp-vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.dpp-igw.id 
+    gateway_id = aws_internet_gateway.dpp-igw.id
   }
+    
 }
 
 resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
   subnet_id = aws_subnet.dpp-public-subnet-01.id
-  route_table_id = aws_route_table.dpp-public-rt.id   
+  route_table_id = aws_route_table.dpp-public-rt.id
 }
 
 resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
-  subnet_id = aws_subnet.dpp-public-subnet-02.id 
-  route_table_id = aws_route_table.dpp-public-rt.id   
+  subnet_id = aws_subnet.dpp-public-subnet-02.id
+  route_table_id = aws_route_table.dpp-public-rt.id
 }
